c3fd358776c8da5737db46988a2320b334715aad
amine-dev1 <159008500+amine-dev1@users.noreply.github.com>
2025-01-29 03:22:16 +0100

    first commit

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
 $servername='localhost';
 $username='useramine';
-$password='amineelidrissi';
+$password='elidrissi2002';
 $database='aminetech';
 $connection=mysqli_connect($servername,$username,$password,$database);
 if($connection->connect_error){
   die("connect erro ".mysqli_connect_error());
 }
 else{
-
+  
 }
-?>
